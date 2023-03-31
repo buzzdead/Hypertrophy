f3bda900868fe187c32c95b71c4cbdc544d17500
Sigmund.Volden <sigmund.volden@student.uib.no>
2023-03-31 18:13:47 +0200

    added some animation

--- src/screens/SideBar.tsx
+++ src/screens/SideBar.tsx
@@ -1,9 +1,9 @@
 // sidebar.tsx
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Animated, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const SidebarWidth = 250;
+const SidebarWidth = 150;
 const SidebarVisibleWidth = 30;
 
 interface SideBarProps {
@@ -12,19 +12,32 @@ interface SideBarProps {
 }
 
 export const SideBar: React.FC<SideBarProps> = ({categories, onFilterChange}) => {
+  const [selectedCategoryAnimations, setSelectedCategoryAnimations] = useState(
+    categories.map(() => new Animated.Value(0)),
+  );
+
   const [sidebarVisible, setSidebarVisible] = useState(false);
   const [translateX] = useState(new Animated.Value(-SidebarWidth));
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
-  const handleCategoryPress = (category: string) => {
+  const handleCategoryPress = (category: string, index: number) => {
     const newSelectedCategories = selectedCategories.includes(category)
       ? selectedCategories.filter(c => c !== category)
       : [...selectedCategories, category];
 
     setSelectedCategories(newSelectedCategories);
     onFilterChange(newSelectedCategories);
-  };
+    console.log(selectedCategoryAnimations);
 
+    Animated.timing(selectedCategoryAnimations[index], {
+      toValue: newSelectedCategories.includes(category) ? 1 : 0,
+      duration: 200,
+      useNativeDriver: false,
+    }).start();
+  };
+  useEffect(() => {
+    setSelectedCategoryAnimations(categories.map(() => new Animated.Value(0)));
+  }, [categories]);
   const handleCloseSidebar = () => {
     Animated.timing(translateX, {
       toValue: -SidebarWidth,
@@ -61,20 +74,31 @@ export const SideBar: React.FC<SideBarProps> = ({categories, onFilterChange}) =>
         <TouchableOpacity
           style={{...styles.sidebarEdge, zIndex: sidebarVisible ? 1001 : -1}}
           onPress={() => (sidebarVisible ? handleCloseSidebar() : handleOpenSidebar())}>
-          <MaterialCommunityIcons adjustsFontSizeToFit name={sidebarVisible ? "chevron-left" : "chevron-right"} size={24} color="#000" />
+          <MaterialCommunityIcons
+            adjustsFontSizeToFit
+            name={"tune-vertical"}
+            size={24}
+            color={sidebarVisible ? "green" : "red"}
+          />
         </TouchableOpacity>
       </View>
       <Text style={styles.sidebarTitle}>Filter</Text>
-      {categories.map(category => (
-        <TouchableOpacity
+      {categories.map((category, index) => (
+        <Animated.View
           key={category}
           style={[
             styles.sidebarItem,
-            {backgroundColor: selectedCategories.includes(category) ? "#eee" : "transparent"},
-          ]}
-          onPress={() => handleCategoryPress(category)}>
-          <Text style={styles.sidebarItemText}>{category}</Text>
-        </TouchableOpacity>
+            {
+              backgroundColor: selectedCategoryAnimations[index]?.interpolate({
+                inputRange: [0, 1],
+                outputRange: ["transparent", "lightgreen"],
+              }),
+            },
+          ]}>
+          <TouchableOpacity onPress={() => handleCategoryPress(category, index)}>
+            <Text style={styles.sidebarItemText}>{category}</Text>
+          </TouchableOpacity>
+        </Animated.View>
       ))}
     </Animated.View>
   );
@@ -86,32 +110,42 @@ const styles = StyleSheet.create({
     top: 0,
     right: -SidebarWidth + SidebarVisibleWidth,
     bottom: 0,
-    backgroundColor: "#f8f8f8",
+    backgroundColor: "#f5f5f5",
     zIndex: 1000,
     width: SidebarWidth - SidebarVisibleWidth,
     borderLeftWidth: 1,
     borderLeftColor: "#ccc",
+    elevation: 5, // Add box shadow on Android
+    shadowColor: "#000", // Add box shadow on iOS
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
   },
   sidebarTitle: {
     fontSize: 24,
-    fontWeight: "bold",
     textAlign: "center",
     marginVertical: 20,
+    fontFamily: "Roboto-Regular",
+    color: "#333",
   },
   sidebarItem: {
-    padding: 15,
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
   },
   sidebarItemText: {
-    fontSize: 18,
+    fontSize: 14,
+    fontWeight: "bold",
+    padding: 11.4,
+    fontFamily: "Roboto-Bold",
+    color: "black",
+    textAlign: "center"
   },
   sidebarEdgeIndicator: {
     position: "absolute",
     top: 35,
-    left: -SidebarVisibleWidth,
+    left: -SidebarVisibleWidth -3,
     zIndex: 1000,
-    backgroundColor: "#ccc", // You can change this color to match your design
+    backgroundColor: "#f5f5f5",
     borderRadius: 4,
   },
   sidebarEdge: {
