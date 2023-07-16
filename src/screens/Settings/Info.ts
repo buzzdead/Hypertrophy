3c750e63b85574db29e334c52eeb643b08c70fa1
buzzdead <sigmundvolden@gmail.com>
2023-07-16 13:01:52 +0200

    updated info

--- src/screens/Settings/Info.ts
+++ src/screens/Settings/Info.ts
@@ -1,10 +1,10 @@
 export const InfoProgress = {
-    PR: "PR stands for Personal Record and takes use of a metric to show you which exercises that stands out as the best exercise for a given day.",
+    PR: "PR stands for Personal Record and takes use of a metric to show you which exercises that stands out as the best exercise for a given day or week.",
     Metric: "Metric is basically the product of weight x reps x sets, but as one does heavier loads it gets adjusted compared to the average metric, if need be.",
     AvgMetric: "All exercise types (I.e Bicep Curl) has an average metric based on your history of doing the exercise type.",
 }
 
 export const InfoHome = {
     Graph: "The graph shows how many exercises you did for each category for the current week (gets reset every week).",
-    Plans: "The plans that are created within the current weeks will be shown on top of home screen (gets reset every week). Can delete these at any time."
+    Plans: "The plans that are created within the current weeks will be shown on top of home screen (gets reset every week). Can delete these at any time without affecting exercises."
 }
